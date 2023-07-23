65ffddb70d66b14c18501353946a9d38b8b4e13b
Lucky Chitundu <87881145+lchitundu@users.noreply.github.com>
2023-07-22 21:03:31 -0500

    added more components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import './App.css';
 
 import Header from './Components/Header';
 import MyRadioButton from './Components/RadioButton';
 import ChartViewer from './Components/ChartViewer';
 import Footer from './Components/Footer';  
 
-import './App.css';
+import AnalysisSettings from './Components/AnalysisSettings';
+import Dashboard from './Components/Dashboard';
+import DataImport from './Components/DataImport';
+import DataVisualization from './Components/Visualization';
+
+
 
 function App() {
   return (
     <div >
-      <Header />
+      {/* <Header />
       <MyRadioButton />
       <div style={{textAlign:'center'}}>
       <ChartViewer />
       </div>
      
+      <Footer /> */}
+      
+      <Header />
+      <DataImport />
+      <AnalysisSettings />
+      <Dashboard />
+      <ChartViewer />
       <Footer />
     </div>
   );
